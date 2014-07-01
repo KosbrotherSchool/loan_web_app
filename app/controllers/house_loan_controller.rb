605eb7925b2e7fd6cae5f99602865e9f13c8f523
Ko LiChung <kerlichung@gmail.com>
2014-07-01 15:05:32 +0800

    add loan case county_id and status_id

--- app/controllers/house_loan_controller.rb
+++ app/controllers/house_loan_controller.rb
@@ -18,13 +18,6 @@ class HouseLoanController < ApplicationController
 		@loan_case = LoanCase.new(loan_case_params)
 
 		@loan_case.address = params[:county] + params[:district] + @loan_case.address
-		
-		County.all.each do |county|
-			county_s = county.name[0..1]
-			if condition
-				
-			end
-		end
 
 		applicant_company_type = ""
 		if params[:company_type_1][:value] != "" 
@@ -47,6 +40,14 @@ class HouseLoanController < ApplicationController
 			end
 		end
 		@loan_case.applicant_company_type = applicant_company_type
+
+		@loan_case.status_id = 1
+		County.all.each do |county|
+			county_s = params[:county][0..1]
+			if county.name.include? county_s
+				@loan_case.county_id = county.id
+			end
+		end
 
 		if @loan_case.save
 			# push_loan_case_to_gdoc(@loan_case.is_dealed, @loan_case.to_buy_price, @loan_case.parking_price, @loan_case.to_loan_price, @loan_case.is_need_grace_period, @loan_case.grace_period_years, @loan_case.to_loan_period_years, @loan_case.other_info, @loan_case.address, @loan_case.layer, @loan_case.building_type, @loan_case.rooms, @loan_case.living_rooms, @loan_case.rest_rooms, @loan_case.building_area, @loan_case.public_area, @loan_case.land_area, @loan_case.building_age, @loan_case.house_decoration, @loan_case.house_condition, @loan_case.is_top_built, @loan_case.top_building_area, @loan_case.parking_type, @loan_case.parking_layer, @loan_case.parking_area, @loan_case.applicant_name, @loan_case.applicant_email, @loan_case.applicant_phone1, @loan_case.applicant_phone2, @loan_case.applicant_age, @loan_case.applicant_company_name, @loan_case.is_applicant_company_founder, @loan_case.applicant_title, @loan_case.applicant_serve_year, @loan_case.applicant_year_earning, @loan_case.applicant_other_earning, @loan_case.applicant_company_type, @loan_case.applicant_is_have_house, @loan_case.applicant_other_house_loan, @loan_case.applicant_other_car_loan, @loan_case.applicant_other_learning_loan, @loan_case.is_applicant_use_revolving_interest, @loan_case.is_applicant_use_check, @loan_case.is_applicant_have_bounce_check)
