0097d1e1857d989c9055eaa702b35ac19ac2946b
Ko LiChung <kerlichung@gmail.com>
2014-07-12 17:22:18 +0800

    change lender default avatar

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -7,7 +7,7 @@ class Lender < ActiveRecord::Base
 	has_many :lender_loan_case_ships
 	has_many :loan_cases, :through => :lender_loan_case_ships
 
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "350x264>", :thumb => "175x132>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_attached_file :card, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
