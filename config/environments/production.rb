f18c7bb65dee671028d26995f493b203f0b8e7c1
Ko LiChung <kerlichung@gmail.com>
2014-07-12 17:38:16 +0800

    add smtp set to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,16 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "e-loan.com.tw",
+    :user_name => "postmaster@e-loan.com.tw",
+    :password => "1ncfxju3mpg4"
+  }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
