8461ed8866702410e1684225c569a15387351a82
Ko LiChung <kerlichung@gmail.com>
2014-07-14 10:44:41 +0800

    change routes and style.css underline

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,19 @@ Rails.application.routes.draw do
   # resources :users
   
   root :to => "house_loan#index"
-  get "about_us" => "house_loan#about_us"
+  get "about-us" => "house_loan#about_us"
   get "index" => "house_loan#index"
 
-  get "join_us" => "house_loan#join_us"
+  get "join-us" => "house_loan#join_us"
   get "faq" => "house_loan#faq"
   get "privacy" => "house_loan#privacy"
   get "term" => "house_loan#term"
-  get "lender_response" => "house_loan#lender_response"
+  # get "lender_response" => "house_loan#lender_response"
 
   post "create_lender" => "house_loan#create_lender"
 
   get "backstage" => "backstage#index"
-  get "apply_mortgage" => "loan_cases#apply_mortgage"
+  get "apply-mortgage" => "loan_cases#apply_mortgage"
   patch "backstage/deliver_mail" => "backstage#deliver_mail"
   patch "backstage/update_evaluate_case" => "backstage#update_evaluate_case"
   patch "backstage/update_delivering_case" => "backstage#update_delivering_case"
