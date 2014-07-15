348ab6aeef2c4d331cd8aa26bcc7504dbbf47dd2
Ko LiChung <kerlichung@gmail.com>
2014-07-15 14:40:14 +0800

    change lender photo sytle

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -7,10 +7,10 @@ class Lender < ActiveRecord::Base
 	has_many :lender_loan_case_ships
 	has_many :loan_cases, :through => :lender_loan_case_ships
 
-	has_attached_file :avatar, :styles => { :medium => "350x264>", :thumb => "175x132>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "350>x264", :thumb => "175>x132" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :card, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :card, :styles => { :medium => "3000>x30", :thumb => "100>x100" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :card, :content_type => /\Aimage\/.*\Z/
 
 	validates :name, :presence => {:message => "請填入姓名"}
