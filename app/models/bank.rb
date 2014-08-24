fb4b1b7ff922cb3b1e08f7343b06f13dba14f4e6
Ko LiChung <kerlichung@gmail.com>
2014-08-24 22:43:12 +0800

    add banks name to url

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,5 +1,6 @@
 class Bank < ActiveRecord::Base
+	extend FriendlyId
+  friendly_id :name
 
 	has_many :bank_products
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813102002) do
+ActiveRecord::Schema.define(version: 20140813102109) do
 
   create_table "bank_products", force: true do |t|
     t.integer  "bank_id"
@@ -46,6 +46,24 @@ ActiveRecord::Schema.define(version: 20140813102002) do
     t.datetime "updated_at"
   end
 
+  create_table "devise_lenders", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "devise_lenders", ["email"], name: "index_devise_lenders_on_email", unique: true, using: :btree
+  add_index "devise_lenders", ["reset_password_token"], name: "index_devise_lenders_on_reset_password_token", unique: true, using: :btree
+
   create_table "lender_county_ships", force: true do |t|
     t.integer  "lender_id"
     t.integer  "county_id"
@@ -70,6 +88,7 @@ ActiveRecord::Schema.define(version: 20140813102002) do
     t.integer  "view_nums"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "onlending_response_id"
   end
 
   create_table "lenders", force: true do |t|
