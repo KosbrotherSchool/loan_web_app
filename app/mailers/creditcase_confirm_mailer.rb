d18997a9bc6b860e286522c84b263045be2940cc
Ko LiChung <kerlichung@gmail.com>
2014-09-15 23:18:26 +0800

    add credit case confirm mailer

--- app/mailers/creditcase_confirm_mailer.rb
+++ app/mailers/creditcase_confirm_mailer.rb
@@ -8,6 +8,6 @@ class CreditcaseConfirmMailer < ActionMailer::Base
   #
   def mail_content(credit_case_id)
     @credit_case = CreditCase.find(credit_case_id)
-    mail to: "\"#{@credit_case.applicant_name}\" <#{@loan_case.applicant_email}>", subject: "EZBANK感謝您的申請[信貸]"
+    mail to: "\"#{@credit_case.applicant_name}\" <#{@credit_case.applicant_email}>", subject: "EZBANK感謝您的申請[信貸]"
   end
 end
